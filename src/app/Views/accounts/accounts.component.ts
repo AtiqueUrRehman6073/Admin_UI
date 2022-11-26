843b8e3c67ac523f5927e9df9aea19b4a2d468bc
AtiqueUrRehman6073 <AtiqueRehman@CareCloud.com>
2022-11-26 10:21:55 +0500

    Loader(minor update 1)

--- src/app/Views/accounts/accounts.component.ts
+++ src/app/Views/accounts/accounts.component.ts
@@ -11,7 +11,7 @@ export class AccountsComponent implements OnInit {
 
   loader: boolean = false;
   loader2: boolean = true;
-  maximizer:boolean = false;
+  maximizer: boolean = false;
   pwidth: number = 0;
   iterator: number = 0;
   iterator2: number = 1;
@@ -26,20 +26,60 @@ export class AccountsComponent implements OnInit {
   pInterval3: any = '';
   pInterval4: any = '';
   tempInterval: any = '';
+  widthChecker: number = 1890;
+  windowWidth: number = 19.5;
+  subtractorCheck: number = 0;
+  rightCheck:number = 20.65;
+  shape3W:number = 4;
+  shape6W:number = 48;
+  boxWidth:number = 20;
   ngOnInit(): void {
+    console.log(window.innerWidth);
+    setTimeout(() => {
+      if(window.innerWidth < 1500){
+        this.systemText2 = 'INITIATING JARVIS OS . . .';
+      }
+      while (this.widthChecker > 1000) {
+        if (this.subtractorCheck === 30) {
+          this.subtractorCheck = 0;
+          this.windowWidth = this.windowWidth - 0.385;
+          this.shape3W = this.shape3W - 0.025;
+          this.rightCheck = this.rightCheck - 0.3499;
+          this.boxWidth = this.boxWidth - 0.655;
+          this.shape6W = this.shape6W - 0.599;
+        }
+        console.clear();
+        console.log(this.windowWidth);
+        if (window.innerWidth === this.widthChecker) {
+          $("#shape4").css('width', this.windowWidth + '%');
+          $("#shape3").css('width', this.shape3W + '%');
+          $("#shape3").css('right', this.rightCheck + '%');
+          $(".col-1").css('width', this.boxWidth + 'px');
+          $("#shape6").css('width', this.shape6W + '%');
+          $("#shape7").css('width', '20.5%');
+          console.log('broken');
+          break;
+        }
+        this.subtractorCheck++;
+        this.widthChecker--;
+      }
+      // if (window.innerWidth < 1500 && window.innerWidth > 1200){
+      //     $("#shape3").css('right','16%');
+      //     $("#shape4").css('width','14.5%');
+      //   }
+    }, 100);
     this.systemtext3[0] = 'Analyzing Login Activity . . .';
     this.systemtext3[1] = 'Analyzing System Files . . .';
     this.systemtext3[2] = 'Retreiving Work Backup . . .';
     this.systemtext3[3] = 'Loading System Files . . .';
     this.systemtext3[4] = 'Analyzing Last Log Off . . .';
     this.systemtext3[5] = 'Loading Complete !';
-    setTimeout(()=>{
+    setTimeout(() => {
       while (this.iterator4 < 66) {
         $("#box-" + this.iterator4).hide();
-        console.log(this.iterator4);
         this.iterator4++;
       }
-    },100);
+    }, 100);
     if (this.iterator2 == 65)
       this.iterator2 = 1;
     $('body').css('background-color', '#000000');
@@ -60,7 +100,7 @@ export class AccountsComponent implements OnInit {
       clearInterval(this.pInterval);
       clearInterval(this.tempInterval);
     }, 3500);
-    setTimeout(()=>{
+    setTimeout(() => {
       this.iterator3 = 0;
       this.pInterval3 = setInterval(() => {
         $("#box-" + this.iterator3).fadeIn(250);
@@ -68,7 +108,7 @@ export class AccountsComponent implements OnInit {
         if (this.iterator3 > 65)
           clearInterval(this.pInterval3);
       }, 80);
-    },7500);
+    }, 7500);
     setTimeout(() => {
       let j = 0;
       this.pInterval4 = setInterval(() => {
@@ -77,7 +117,7 @@ export class AccountsComponent implements OnInit {
           j++;
           if (j >= this.systemtext3[this.i].length) {
             clearInterval(this.tempInterval);
-            j=0;
+            j = 0;
             this.i++;
             $('#statText').append('<p>');
           }
@@ -89,43 +129,43 @@ export class AccountsComponent implements OnInit {
         }
       }, 950);
     }, 500);
-    setTimeout(()=>{
-      this.loader2 = false;
-    },15000);
+    setTimeout(() => {
+      this.loader2 = true;
+    }, 15000);
 
   }
 
-  maximize(id:any){
-    if (this.maximizer){
-      $("#"+id).css('width','40%');
-      $("#"+id).css('height','50vh');
-      $("#"+id).css('top','25%');
-      $("#"+id).css('left','30%');
-      $("#leftmenu").css('opacity','1');
-      $("#rightmenu").css('opacity','1');
-      $("#date_time").css('opacity','1');
-      $("#date").css('opacity','1');
-      $("#time").css('opacity','1');
-      $("#particle2").css('opacity','1');
-      $("#ccp1").css('opacity','1');
-      $("#ccp2").css('opacity','1');
+  maximize(id: any) {
+    if (this.maximizer) {
+      $("#" + id).css('width', '40%');
+      $("#" + id).css('height', '50vh');
+      $("#" + id).css('top', '25%');
+      $("#" + id).css('left', '30%');
+      $("#leftmenu").css('opacity', '1');
+      $("#rightmenu").css('opacity', '1');
+      $("#date_time").css('opacity', '1');
+      $("#date").css('opacity', '1');
+      $("#time").css('opacity', '1');
+      $("#particle2").css('opacity', '1');
+      $("#ccp1").css('opacity', '1');
+      $("#ccp2").css('opacity', '1');
     }
-    else{
-      $("#"+id).css('width','100%');
-      $("#"+id).css('height','100vh');
-      $("#"+id).css('top','0%');
-      $("#"+id).css('left','0%');
-      $("#leftmenu").css('opacity','0.02');
-      $("#rightmenu").css('opacity','0.02');
-      $("#date_time").css('opacity','0.02');
-      $("#date").css('opacity','0.02');
-      $("#particle2").css('opacity','0.02');
-      $("#ccp1").css('opacity','0.02');
-      $("#ccp2").css('opacity','0.02');
+    else {
+      $("#" + id).css('width', '100%');
+      $("#" + id).css('height', '100vh');
+      $("#" + id).css('top', '0%');
+      $("#" + id).css('left', '0%');
+      $("#leftmenu").css('opacity', '0.02');
+      $("#rightmenu").css('opacity', '0.02');
+      $("#date_time").css('opacity', '0.02');
+      $("#date").css('opacity', '0.02');
+      $("#particle2").css('opacity', '0.02');
+      $("#ccp1").css('opacity', '0.02');
+      $("#ccp2").css('opacity', '0.02');
     }
     this.maximizer = !this.maximizer;
   }
-  closeWindow(id:any){
-    $("#"+id).hide(150);
+  closeWindow(id: any) {
+    $("#" + id).hide(150);
   }
 }
